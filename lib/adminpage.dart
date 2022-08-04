b0b66b20391ead1030c85f8e90fa66ef4f344db1
Syed Bilal Abbas <abbasb000@gmail.com>
2022-08-05 03:06:18 +0500

    Edit attendance option added in User Attendance

--- lib/adminpage.dart
+++ lib/adminpage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,24 +27,6 @@ class Database {
   Database(this.parent);
 }
 
-int getMaxLength(List<Database> db){
-  int count = 0;
-  for (var i in db){
-    for (var j in i.children){
-      count++;
-    }
-  }
-  return count;
-}
-
-void setUserAttendance(List<Database> list, List<UserAttendance> userAttendanceList){
-  userAttendanceList.clear();
-  for(var e in list){
-    for (var e2 in e.children) {
-      userAttendanceList.add(UserAttendance("", e.parent, e2.key.replaceFirst("Attendance-", ""), e2.value));
-    }
-  }
-}
 
 
 // class UserAttendance {
@@ -58,7 +41,7 @@ void setUserAttendance(List<Database> list, List<UserAttendance> userAttendanceL
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Attendance Management System';
 
   @override
   Widget build(BuildContext context) {
@@ -87,6 +70,8 @@ class _AdminPageState extends State<AdminPage> {
 
   static List<Database> dbList = [];
 
+  // static String newValue = "Present";
+
 
 
 
@@ -98,41 +83,7 @@ class _AdminPageState extends State<AdminPage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static final List<Widget> _widgetOptions = <Widget>[
-    Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              itemCount: userAttendanceList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  shadowColor: Colors.grey.shade300,
-                  child: ListTile(
-                    title: Text(
-                      userAttendanceList[index].username,
-                    ),
-                    subtitle: Text(
-                      userAttendanceList[index].date,
-                    ),
-                    trailing: Text(
-                      userAttendanceList[index].attendance,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ]
-    ),
-    const Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    const Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
+
 
   void _onItemTapped(int index) {
     setState(() {
@@ -175,6 +126,59 @@ class _AdminPageState extends State<AdminPage> {
     return true;
   }
 
+  getWidget(){
+    final List<Widget> _widgetOptions = <Widget>[
+      Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                itemCount: userAttendanceList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    shadowColor: Colors.grey.shade300,
+                    child: ListTile(
+                      title: Text(
+                        userAttendanceList[index].username,
+                      ),
+                      subtitle: Text(
+                        userAttendanceList[index].date,
+                      ),
+                      // trailing: Text(
+                      //   userAttendanceList[index].attendance,
+                      // ),
+                      trailing: DropdownButton<String>(
+                        value: userAttendanceList[index].attendance,
+                        items: <String>['Present', 'Absent'].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          userAttendanceList[index].attendance = newValue!;
+                          FirebaseDatabase.instance.ref("UserInfo").child(userAttendanceList[index].username).child("Attendance-${userAttendanceList[index].date}").set(newValue);
+                          setState(() {});
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ]
+      ),
+      const Text(
+        'Index 1: Business',
+        style: optionStyle,
+      ),
+      const Text(
+        'Index 2: School',
+        style: optionStyle,
+      ),
+    ];
+    return _widgetOptions[_selectedIndex];
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -189,7 +193,7 @@ class _AdminPageState extends State<AdminPage> {
                 title: const Text('Attendance Management System'),
               ),
               body: Center(
-                child: _widgetOptions.elementAt(_selectedIndex),
+                child: getWidget(),
               ),
               bottomNavigationBar: BottomNavigationBar(
                 items: const <BottomNavigationBarItem>[
@@ -210,6 +214,16 @@ class _AdminPageState extends State<AdminPage> {
                 selectedItemColor: Colors.amber[800],
                 onTap: _onItemTapped,
               ),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: const Color(0xff03dac6),
+                foregroundColor: Colors.black,
+                tooltip: 'Add Attendance',
+                onPressed: () {
+                  // Respond to button press
+                  showToast("Add Attendance Pressed");
+                },
+                child: const Icon(Icons.add),
+              ),
             );
           } else {
             return Scaffold(
@@ -223,6 +237,39 @@ class _AdminPageState extends State<AdminPage> {
           }
         }
     );
+  }
+
+  static void showToast(String str){
+    Fluttertoast.showToast(
+        msg: str,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.yellow
+    );
+  }
+
+
+  int getMaxLength(List<Database> db){
+    int count = 0;
+    for (var i in db){
+      for (var j in i.children){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  void setUserAttendance(List<Database> list, List<UserAttendance> userAttendanceList){
+    userAttendanceList.clear();
+    for(var e in list){
+      for (var e2 in e.children) {
+        if (e2.key.contains("Attendance-")) {
+          userAttendanceList.add(UserAttendance(
+              "", e.parent, e2.key.replaceFirst("Attendance-", ""), e2.value));
+        }
+      }
+    }
   }
 }
 
